56ab1c7b91847e21ac29c30d88e9df4c0b702593
Nick <teuemail@gmail.com>
2026-06-23 10:06:25 -0300

    .

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const rotasApi = require('./src/routes/apiRoutes');
 const rotasCategorias = require('./src/routes/categoriaRoutes');
 const rotasUsuarios = require('./src/routes/usuarioRoutes');
 const authMiddleware = require('./src/middleware/authMiddleware');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./src/swagger/swagger.json');
 
 const app = express();
 
@@ -21,6 +23,9 @@ app.use('/api', rotasApi);
 // Rotas protegidas
 app.use('/api/categorias', authMiddleware, rotasCategorias);
 app.use('/api/usuarios', authMiddleware, rotasUsuarios);
+
+// Swagger UI
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
